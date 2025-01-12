49c92f8d267c34c03b615528c604eaf557100f19
MohammedAldaif <mohammedaldaif71@gmail.com>
2025-01-12 13:44:22 +0300

    Add firebase authentication , file:firebase.js

---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARey139NlrteQ5XYZBjPf1QyKomxD7aes",
+  authDomain: "minimalist-inventory-tracker.firebaseapp.com",
+  projectId: "minimalist-inventory-tracker",
+  storageBucket: "minimalist-inventory-tracker.firebasestorage.app",
+  messagingSenderId: "1051800508790",
+  appId: "1:1051800508790:web:615671690a081f88f00257"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
